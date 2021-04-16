b56ab4719217d552970d710cc6f8e5c5032c8af2
Eric Hurley <erichurley421@gmail.com>
2021-04-16 09:47:25 -0400

    removed switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AboutMe from "./components/AboutMe";
@@ -11,17 +11,15 @@ function App() {
   return (
     <Router>
       <Header />
-        <Switch >
-          <Route exact path="/">
-            <AboutMe />
-          </Route>
-          <Route exact path="/Projects">
-            <Projects />
-          </Route>
-          <Route exact path="/Contact">
-            <Contact />
-          </Route>
-      </Switch>
+      <Route exact path="/">
+        <AboutMe />
+      </Route>
+      <Route exact path="/Projects">
+        <Projects />
+      </Route>
+      <Route exact path="/Contact">
+        <Contact />
+      </Route>
       <Footer />
     </Router>
   );
